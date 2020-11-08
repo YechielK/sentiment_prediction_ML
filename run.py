aa2a4611ccbebc9170a7777ee5f5ae253103a44b
YechielK <thespecialk1@gmail.com>
2020-11-07 23:39:56 -0500

    add files

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "this is my flask app"
+    return render_template('index.html', Title="Project")
+
+@app.route("/user-input")
+def handleUserInput():
+    userInput = request.args.get("sentence")
+    print(userInput)
+    return None

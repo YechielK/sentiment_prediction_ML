d37ac84cdb2f13d30a2f032250bc3513fa96581d
YechielK <thespecialk1@gmail.com>
2020-11-26 10:34:00 -0500

    updated frontend

--- lib/predict.py
+++ lib/predict.py
@@ -1,5 +1,6 @@
 from pickle import load, loads
 from lib.sentence_cleaning import text_pipeline
+from flask import g
 with open("pickles/model.pickle", mode="rb") as pickle_file:
     pickled_model = load(pickle_file)
     model = loads(pickled_model)
@@ -11,8 +12,9 @@ with open("pickles/vectorizer.pickle", mode="rb") as pickle_file:
 
 def predict(sentence):
     sentence = text_pipeline(sentence)
-
+    g.lemmed = sentence
     # This transforms your documents into vectors.
     X = vectorizer.transform([sentence])
-
+    g.X = X
+    g.model = model
     return model.predict(X)
